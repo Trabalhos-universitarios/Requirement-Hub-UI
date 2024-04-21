dd6e378309db561d6313f386befe538deb7b50cc
eliasfernandescout <eliasfernandescout@gmail.com>
2024-04-21 13:27:22 -0300

    feature: adicionando axios request

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AxiosService} from "../../axios.service";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  data: string[] = [];
+  constructor(private axios: AxiosService) {
+    this.axios.request('post', '/login', {}).then((res) => {
+      this.data = res.data;
+    });
+  }
 
 }
