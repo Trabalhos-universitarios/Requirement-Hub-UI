ceeffe0ed72137621a8b088c45b1dcaa66de2289
JohnnyCarvalho <johnnycarvalhodev@gmail.com>
2024-05-04 22:17:22 -0300

    RH-39 - Commit parcial página principal.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,4 +26,11 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
   });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
+  });
 });
