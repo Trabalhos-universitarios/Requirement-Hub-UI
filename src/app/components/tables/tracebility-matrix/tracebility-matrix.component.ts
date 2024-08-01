c749978f7f1419b84311a466fecc7571ad3310ab
lucalem0309 <lucalem0309@gmail.com>
2024-08-01 00:27:42 -0300

    componente matriz

--- src/app/components/tables/tracebility-matrix/tracebility-matrix.component.ts
+++ src/app/components/tables/tracebility-matrix/tracebility-matrix.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatrixService } from 'src/app/services/matrix/matrix.service';
 
 @Component({
   selector: 'app-tracebility-matrix',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class TracebilityMatrixComponent {
 
+  dataSource = [];
+
+  constructor(
+      private traceabilityService: MatrixService){this.getData();}
+
+  getData() {
+      this.traceabilityService.getTraceabilityMatrix().subscribe((matrix: []) => {
+          this.dataSource = matrix;
+      });
+  }
+
+  getCellStyles(cell: any): any {
+    let backgroundColor;
+    if (cell === null) {
+      backgroundColor = 'black';
+    } else if (cell === "") {
+      backgroundColor = 'white';
+    } 
+    else if (cell === "X") {
+      backgroundColor = 'lightgreen';
+    }
+      else {
+      backgroundColor = 'gray';
+    }
+    return {
+      'background-color': backgroundColor
+    };
+  }
 }
