223350da2225051a69ff3994b14de08d908d02b7
lucalem0309 <lucalem0309@gmail.com>
2024-08-08 21:07:48 -0300

    correcao de zeramento de token

--- src/app/auth/token-interceptor.ts
+++ src/app/auth/token-interceptor.ts
@@ -7,9 +7,10 @@ export class TokenInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
     if (token) {
+      const formattedToken = token.replace(/"/g, ''); 
       request = request.clone({
         setHeaders: {
-          Authorization: `Basic ${token}`
+          Authorization: `Bearer ${formattedToken}`
         }
       });
     }
