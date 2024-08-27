c098114d45becb08813f3a48fb932ab0c89fadc1
lucalem0309 <lucalem0309@gmail.com>
2024-08-26 22:17:10 -0300

    create edit requirement artifact

--- src/app/utils/artifact-type-list-util.ts
+++ src/app/utils/artifact-type-list-util.ts
@@ -4,14 +4,17 @@ export const ARTIFACT_TYPE_LIST: ArtifactType[] =
     [
         {identifier: 'UC', name: 'Use Case'},
         {identifier: 'UH', name: 'Use History'},
-        {identifier: 'TP', name: 'Test Plan'},
-        {identifier: 'CD', name: 'Configuration Documentation'},
-        {identifier: 'DM', name: 'Data Models'},
-        {identifier: 'UI', name: 'UI Prototypes'},
-        {identifier: 'UM', name: 'User Manual'},
-        {identifier: 'TM', name: 'Technical Manual'},
-        {identifier: 'UML', name: 'Unified Modeling Language (UML) Diagrams'},
-        {identifier: 'NFR', name: 'Non-Functional Requirements Documentation'},
-        {identifier: 'FR', name: 'Functional Requirements Documentation'},
-        {identifier: 'BR', name: 'Business Rules Documentation'}
+        {identifier: 'CT', name: 'Test Case'},
+        {identifier: 'MD', name: 'Implementation module'},
+        {identifier: 'DC', name: 'Document'},
+        //{identifier: 'TP', name: 'Test Plan'},
+        //{identifier: 'CD', name: 'Configuration Documentation'},
+        //{identifier: 'DM', name: 'Data Models'},
+        //{identifier: 'UI', name: 'UI Prototypes'},
+        //{identifier: 'UM', name: 'User Manual'},
+        //{identifier: 'TM', name: 'Technical Manual'},
+        //{identifier: 'UML', name: 'Unified Modeling Language (UML) Diagrams'},
+        //{identifier: 'NFR', name: 'Non-Functional Requirements Documentation'},
+        //{identifier: 'FR', name: 'Functional Requirements Documentation'},
+        //{identifier: 'BR', name: 'Business Rules Documentation'}
     ];
