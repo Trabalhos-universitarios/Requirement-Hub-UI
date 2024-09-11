33b839ab78c236135f4c3206f543dce204234dce
lucalem0309 <lucalem0309@gmail.com>
2024-09-11 19:25:53 -0300

    hotfix css dashboard

--- src/app/core/project-status/project-status.component.ts
+++ src/app/core/project-status/project-status.component.ts
@@ -4,6 +4,9 @@ import { ProjectsService } from 'src/app/services/projects/projects.service';
 import { RequirementsDataModel } from 'src/app/models/requirements-data-model';
 import { ProjectDataModel } from 'src/app/models/project-data-model';
 import { Color, ScaleType } from '@swimlane/ngx-charts';
+import { SpinnerService } from 'src/app/services/spinner/spinner.service';
+import { ThemeService } from 'src/app/services/theme/theme.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-project-status',
@@ -11,41 +14,85 @@ import { Color, ScaleType } from '@swimlane/ngx-charts';
   styleUrls: ['./project-status.component.scss']
 })
 export class ProjectStatusComponent implements OnInit {
-  projects: ProjectDataModel[] = [];  // Lista de projetos
-  selectedProjectId: number | null = null;  // Projeto selecionado
+  projects: ProjectDataModel[] = [];
+  selectedProjectId: number | null = null;
   pieChartData: any[] = [];  // Para status dos requisitos
   barChartData: any[] = [];  // Para quantidade de requisitos por responsável
   colorScheme: Color = {
-    name: 'dark',  // Nome do esquema de cores
+    name: this.themeService.isDarkMode() ? 'dark': 'light', 
     selectable: true,
     group: ScaleType.Ordinal,
-    domain: ['#1E88E5', '#D32F2F', '#FFC107', '#43A047'] // Paleta de cores ajustada
+    domain: ['#1E88E5', '#D32F2F', '#FFC107', '#43A047']
   };
+
+  // Variável de controle para o estado de carregamento
+  isLoading: boolean = false;
+  themeSubscription: Subscription |undefined;
 
   constructor(
     private requirementsService: RequirementsService,
-    private projectsService: ProjectsService
+    private projectsService: ProjectsService,
+    private spinnerService: SpinnerService,
+    private themeService: ThemeService
   ) {}
 
   ngOnInit() {
+    this.spinnerService.start();
+    this.setChartColors();  // Define as cores iniciais
     this.loadProjects();
+
+    // Subscreve às mudanças no tema para atualizar o esquema de cores dos gráficos dinamicamente
+    this.themeSubscription = this.themeService.getColorThemeObservable().subscribe(() => {
+      this.setChartColors();
+    });
   }
 
-  // Função para carregar todos os projetos
-  async loadProjects() {
-    this.projects = await this.projectsService.getProjects();
-    if (this.projects.length > 0) {
-      this.selectedProjectId = this.projects[0].id;  // Seleciona o primeiro projeto por padrão
-      this.loadProjectData(this.selectedProjectId);  // Carrega os dados do projeto selecionado
+  ngOnDestroy() {
+    // Cancela a subscrição ao trocar de componente
+    if (this.themeSubscription) {
+      this.themeSubscription.unsubscribe();
     }
   }
 
-  // Função que dispara quando o projeto é alterado
-  onProjectChange(projectId: number) {
-    this.loadProjectData(projectId);  // Recarrega os dados com base no novo projeto selecionado
+  setChartColors(): void {
+    const isDarkMode = this.themeService.isDarkMode();
+    this.colorScheme = {
+      name: isDarkMode ? 'dark' : 'light',
+      selectable: true,
+      group: ScaleType.Ordinal,
+      domain: isDarkMode ? ['#90caf9', '#f48fb1', '#ffb74d'] : ['#1E88E5', '#D32F2F', '#FFC107', '#43A047']
+    };
   }
 
-  // Função para carregar os dados do projeto selecionado
+  async loadProjects() {
+    const allProjects = await this.projectsService.getProjects();
+    
+    const projectsWithRequirements = await Promise.all(
+      allProjects.map(async (project) => {
+        const requirements = await this.requirementsService.getRequirementsByProjectId(project.id);
+        return requirements.length > 0 ? project : null;
+      })
+    );
+
+    this.projects = projectsWithRequirements.filter(project => project !== null) as ProjectDataModel[];
+  
+    if (this.projects.length > 0) {
+      this.selectedProjectId = this.projects[0].id;
+      await this.loadProjectData(this.selectedProjectId);
+      this.spinnerService.stop();
+    } else {
+      this.selectedProjectId = null;
+      this.pieChartData = [];
+      this.barChartData = [];
+      console.log('Nenhum projeto com requisitos encontrado.');
+      this.spinnerService.stop();
+    }
+  }
+
+  async onProjectChange(projectId: number) {
+    await this.loadProjectData(projectId);
+  }
+
   async loadProjectData(projectId: number) {
     await this.getRequirementsStatusByProject(projectId);
     await this.getRequirementsByResponsible(projectId);
@@ -61,76 +108,47 @@ export class ProjectStatusComponent implements OnInit {
         }
         return acc;
       }, {});
-  
+
       this.pieChartData = Object.keys(statusCount).map(status => ({
         name: `${status}`,
         value: statusCount[status]
       }));
     } else {
-      this.pieChartData = [];  // Limpa os dados se não houver requisitos
+      this.pieChartData = []; 
     }
   }
 
   async getRequirementsByResponsible(projectId: number) {
-    // Obtém os requisitos do projeto selecionado
     const data: RequirementsDataModel[] = await this.requirementsService.getRequirementsByProjectId(projectId);
   
-    // Log para verificar se os requisitos estão sendo obtidos corretamente
-    console.log("Requisitos do projeto selecionado:", data);
-  
     if (data && data.length > 0) {
-      // Extrai os IDs dos requisitos do projeto selecionado
       const requirementIds = data.map(req => req.id);
-      console.log("IDs dos requisitos do projeto:", requirementIds);
-  
-      // Obtém todos os responsáveis de todos os requisitos
       const responsiblesData = await this.requirementsService.getAllRequirementResponsibles();
-  
-      // Log para verificar se os dados de responsáveis estão sendo retornados
-      console.log("Todos os responsáveis obtidos:", responsiblesData);
-  
+
       if (responsiblesData && responsiblesData.length > 0) {
-        // Filtra os responsáveis apenas pelos requisitos que pertencem ao projeto selecionado
         const filteredResponsibles = responsiblesData.filter((responsible: any) => 
-          requirementIds.includes(responsible[0]) // Alterado aqui para acessar o requirement_id corretamente
+          requirementIds.includes(responsible[0])
         );
-  
-        // Log para verificar os responsáveis filtrados
-        console.log("Responsáveis filtrados (apenas os do projeto selecionado):", filteredResponsibles);
-  
-        // Conta quantos requisitos estão associados a cada responsável
+
         const responsibleCount: { [key: string]: { name: string, count: number } } = filteredResponsibles.reduce((acc: { [key: string]: { name: string, count: number } }, responsible: any) => {
-          const userId = responsible[1]; // Alterado para acessar o user_id corretamente
-          const userName = responsible[2]; // Alterado para acessar o nome do usuário corretamente
+          const userId = responsible[1]; 
+          const userName = responsible[2];
           if (!acc[userId]) {
             acc[userId] = { name: userName, count: 0 };
           }
           acc[userId].count += 1;
           return acc;
         }, {});
-  
-        // Log para verificar a contagem dos responsáveis
-        console.log("Contagem de responsáveis:", responsibleCount);
-  
-        // Converte os responsáveis para o formato esperado pelo gráfico
+
         this.barChartData = Object.keys(responsibleCount).map(userId => ({
-          name: responsibleCount[userId].name,  // Usando o nome real do responsável
+          name: responsibleCount[userId].name,
           value: responsibleCount[userId].count
         }));
-  
-        // Log para verificar os dados finais do gráfico
-        console.log("Dados para o gráfico de responsáveis:", this.barChartData);
       } else {
-        // Limpa os dados se não houver responsáveis encontrados
         this.barChartData = [];
-        console.log("Nenhum responsável encontrado para os requisitos do projeto.");
       }
     } else {
-      // Limpa os dados se não houver requisitos para o projeto selecionado
       this.barChartData = [];
-      console.log("Nenhum requisito encontrado para o projeto selecionado.");
     }
   }
-  
-  
 }
