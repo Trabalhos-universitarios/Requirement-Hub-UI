35a0efb3c1a59710c3122498784c0730e9910c9e
eliasfernandescout <eliasfernandescout@gmail.com>
2024-09-13 10:36:37 -0300

    deploy

--- src/environment/environment-local.ts
+++ src/environment/environment-local.ts
@@ -1,6 +1,6 @@
 export const environmentLocal = {
     production: false,
-    springUrl: '54.235.228.16:8080',
+    springUrl: 'http://54.235.228.16:8080',
     fakeUrl: 'http://localhost:3000',
     nodeServerUrl: 'http://localhost:8081',
 };
