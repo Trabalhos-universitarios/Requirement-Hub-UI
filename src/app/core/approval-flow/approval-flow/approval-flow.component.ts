9cea620e51ef2aa47722b1a3850ed321fe1bff7a
lucalem0309 <lucalem0309@gmail.com>
2024-09-21 15:54:35 -0300

    create component approval-flow

---
+++ src/app/core/approval-flow/approval-flow/approval-flow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-approval-flow',
+  templateUrl: './approval-flow.component.html',
+  styleUrls: ['./approval-flow.component.scss']
+})
+export class ApprovalFlowComponent {
+
+}
